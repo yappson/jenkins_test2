35dfa753ab8eac7ff3aba353a21d558ff4e1cc66
Yatendra Singh <11352997+yappson@users.noreply.github.com>
2023-11-27 10:29:42 +0530

    Update EasyNotesApplication.java

--- src/main/java/com/example/easynotes/EasyNotesApplication.java
+++ src/main/java/com/example/easynotes/EasyNotesApplication.java
@@ -6,8 +6,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class EasyNotesApplication {
+public class EasyNotesApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EasyNotesApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(EasyNotesApplication.class, args);
 	}
